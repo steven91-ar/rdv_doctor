15596eb56aad58279076ac0150aad3c8a988d855
steven9work <72232262+steven91-ar@users.noreply.github.com>
2025-04-14 12:04:56 +0200

    projet final

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import HomeTemplateView, AppointmentTemplateView, ManageAppointmentTemplateView
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", HomeTemplateView.as_view(), name="home"),
+    path("make-an-appointment/", AppointmentTemplateView.as_view(), name="appointment"),
+    path("manage-appointments/", ManageAppointmentTemplateView.as_view(), name="manage"),
 ]
